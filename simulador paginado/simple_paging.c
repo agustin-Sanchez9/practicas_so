8a067c821c6fb9d9b2fc9e0f6a166e1b277d34d6
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-09-16 20:26:46 -0300

    update

--- simulador paginado/simple_paging.c
+++ simulador paginado/simple_paging.c
@@ -14,7 +14,6 @@ ej:
 */
 
 
-
 struct data_frame {
     unsigned char state; // 0 = free, 1 = ocuppied
     unsigned char owner; // 0 = os, 1 = user
@@ -27,9 +26,17 @@ struct data_memory{
     int size_frame;
     int nframe; // number of existing frames
     int nframe_os; // number of os frames
+    int nframes_free; // number of free frames
 } data_memory;
 
 
+struct data_process{
+    // pid_t pid
+    // int nframes_proc (number of pages of the process)
+    // int proc_frames[] (integers array to save the pages the process owns)
+} data_process;
 
 // funcion ocupar frames
 // funcion mostrar datos
+// a la hora de agregar un proceso chequear si entra en los frames disponibles
+// bloque de control de procesos "bcp"
